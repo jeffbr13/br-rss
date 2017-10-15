45542f012b3dc6d089bc991529523a4ea6401b35
Ben Jeffrey <mail@benjeffrey.net>
2017-10-15 12:21:03 +0100

    Use relative import in command.

--- br_rss/boilerroomtv/management/commands/scrape_all.py
+++ br_rss/boilerroomtv/management/commands/scrape_all.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from boilerroomtv.tasks import scrape_genres, scrape_all_recordings, scrape_channels
+from ...tasks import scrape_genres, scrape_all_recordings, scrape_channels
 
 
 class Command(BaseCommand):
